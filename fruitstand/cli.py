43c8b2e40e85c5c1b947d6d2c08f9fd386f7f5c0
gareth-dd <gareth@deckarddesigns.com>
2025-01-12 17:57:27 +0000

    restructuring

---
+++ fruitstand/cli.py
@@ -0,0 +1,13 @@
+import sys
+
+from utils import argument_utils
+from controllers import main
+
+def start():
+    command = sys.argv[1]
+
+    parser = argument_utils.getArgumentParser(command)
+
+    args, unknown = parser.parse_known_args()
+
+    main.start(command, args)
