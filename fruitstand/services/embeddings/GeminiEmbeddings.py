3e9003cd2da8a13ad90921bde6632fe420627a0e
gareth-dd <gareth@deckarddesigns.com>
2025-01-16 13:39:52 +0000

    adding gemini embeddings

---
+++ fruitstand/services/embeddings/GeminiEmbeddings.py
@@ -0,0 +1,14 @@
+import google.generativeai as genai
+from fruitstand.services.embeddings.EmbeddingsService import EmbeddingsService
+
+class GeminiEmbeddings(EmbeddingsService):
+    def __init__(self, api_key: str):
+        genai.configure(api_key=api_key)
+
+    def embed(self, model: str, text: str) -> list:
+        response = genai.embed_content(
+        model=model,
+        content=text)
+
+        # Extract the embeddings from the response
+        return response.data[0].embedding
